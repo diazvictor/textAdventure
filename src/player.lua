9846290fdd1cb0d237e93606b9c714b44d6140f6
diazvictor <diaz.victor@openmailbox.org>
2020-07-10 00:14:19 -0400

    modulo para la interacción con el jugador

---
+++ src/player.lua
@@ -0,0 +1,17 @@
+--[[
+ @package	textAdventure
+ @filename  player.lua
+ @version   1.0
+ @author    Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
+ @date      10.06.2020 14:07:45 -04
+]]--
+
+player = {}
+player.name = ''
+player.hp = 100
+player.score = 0
+player.dead = false
+
+player.inventory = {}
+
+return player
