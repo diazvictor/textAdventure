0122bfaa90fc5423ceed72fc3760524514f255c3
diazvictor <diaz.victor@openmailbox.org>
2020-07-10 03:41:56 -0400

    modulo de requerimiento

---
+++ src/textAdventure.lua
@@ -0,0 +1,13 @@
+--[[
+ @package   textAdventure
+ @filename  textAdventure.lua
+ @version   1.0
+ @author    Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
+ @date      07.07.2020 12:15:15 -04
+]]--
+
+require 'excuses'
+require 'player'
+require 'text'
+require 'room'
+require 'game'
