61019aaa76e6e63c5ab9f332dddf9f3253ec9e5e
diazvictor <diaz.victor@openmailbox.org>
2020-07-09 23:41:56 -0400

    add parse input

--- src/game.lua
+++ src/game.lua
@@ -10,4 +10,49 @@ game = {}
 game.running = true
 game.state = "intro"
 
+game.actions = {}
+game.verbs = {
+	drop_syn = {
+		'drop (.+)'
+	},
+	goto_syn = {
+		"go (.+)"
+	},
+	help_syn = {
+		'help'
+	},
+	look_syn = {
+		'look (.+)'
+	},
+	take_syn = {
+		'take (.+)'
+	},
+	inventory_syn = {
+		'inventory'
+	},
+	exit_syn = {
+		'exit'
+	},
+	exits_syn = {
+		'exits'
+	}
+}
+
+function game.doaction(action, param)
+	game.actions[action](param)
+end
+
+function parse(text)
+	local text = text:lower()
+	for action,valids in pairs(game.verbs) do
+		for i,valid in ipairs(valids) do
+			local result = string.match(text, valid)
+			if result then
+				game.doaction(action, result)
+				return
+			end
+		end
+	end
+end
+
 return game
