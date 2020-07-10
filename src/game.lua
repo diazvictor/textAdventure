0065fc8ce7d28f74600ca43a86648ea418b608f4
diazvictor <diaz.victor@openmailbox.org>
2020-07-09 23:36:07 -0400

    add game module

---
+++ src/game.lua
@@ -0,0 +1,13 @@
+--[[--
+ @package   textAdventure
+ @filename  game.lua
+ @version   1.6
+ @autor     Díaz Urbaneja Víctor Eduardo Diex
+ @date      23.06.2020 15:20:00 -04
+]]
+
+game = {}
+game.running = true
+game.state = "intro"
+
+return game
